37479fdba9591d94dbae952ed2bb5579f49f5569
Bruno Cabral <cabral.brc@gmail.com>
2025-10-06 00:04:18 -0700

    fix Font::render command

--- src/rygame_cl_Font.cpp
+++ src/rygame_cl_Font.cpp
@@ -16,9 +16,9 @@ rg::font::Font::Font(rl::Font font, const float font_size) : font(font), font_si
 {
 }
 
-rg::font::Font::Font(Font &&other) noexcept : font(other.font), font_size(other.font_size)
+rg::font::Font::Font(Font &&other) noexcept : Font()
 {
-    other.font = rl::GetFontDefault();
+    *this = std::move(other);
 }
 
 rg::font::Font &rg::font::Font::operator=(Font &&other) noexcept
@@ -38,28 +38,22 @@ rg::Surface rg::font::Font::render(
         const char *text, const rl::Color color, const float spacing, const rl::Color bg,
         const float padding_width, const float padding_height) const
 {
-    // const rl::Image imageText = ImageTextEx(font, text, font_size, spacing, color);
-    // const rl::Texture texture = LoadTextureFromImageSafe(imageText);
-    //
     auto [w, h] = rl::MeasureTextEx(font, text, font_size, spacing);
     const int surfWidth = w + padding_width;
     const int surfHeight = h + padding_height;
 
     auto result = Surface(surfWidth, surfHeight);
     result.Fill(bg);
-    // result.Blit(
-    //         texture, {padding_width / 2.0f, padding_height / 2.0f},
-    //         {0, 0, texture.width, -texture.height});
+    auto f = font;
+    auto fs = font_size;
     result.draw_cmds.emplace_back(
-            [this, text, padding_width, padding_height, spacing, color]
+            [f, fs, text, padding_width, padding_height, spacing, color]
             {
                 rl::DrawTextEx(
-                        font, text, {padding_width / 2.0f, padding_height / 2.0f}, font_size,
+                        f, text, {padding_width / 2.0f, padding_height / 2.0f}, fs,
                         spacing, color);
             });
 
-    // UnloadTextureSafe(texture);
-    // UnloadImage(imageText);
     return result;
 }
 
